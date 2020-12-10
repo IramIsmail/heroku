479a5beb77f9dbdc2d97be97fb0880f624be6859
Iram Ismail <iram.8859@gmail.com>
2020-12-10 12:53:16 +0530

    Initial Commit

---
+++ HerokuApp/src/test/java/com/application/heroku/HerokuAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.heroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
