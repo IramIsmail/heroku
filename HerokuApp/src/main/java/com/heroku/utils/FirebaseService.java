853bdb73f688fd6ae00e58c17ed78083028baded
Iram Ismail <iram.8859@gmail.com>
2021-02-09 12:17:03 +0530

    Comit

--- HerokuApp/src/main/java/com/heroku/utils/FirebaseService.java
+++ HerokuApp/src/main/java/com/heroku/utils/FirebaseService.java
@@ -1,35 +1,23 @@
 package com.heroku.utils;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.annotation.PostConstruct;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
-
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
-import com.google.firebase.messaging.BatchResponse;
-import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.MulticastMessage;
-import com.google.firebase.messaging.Notification;
 
 @Service
 public class FirebaseService {
 	 @PostConstruct
-	    public void initialize() throws Exception {
-	            InputStream serviceAccount =this.getClass().getClassLoader().getResourceAsStream("./notifycheck-dfc1f-firebase-adminsdk-ca4t0-b33b5e3d92.json");
-	        	
+	    public void initialize() throws IOException {
+	            InputStream serviceAccount =this.getClass().getClassLoader().getResourceAsStream("./notifycheck-dfc1f-firebase-adminsdk-ca4t0-bf98b9dba7.json");	        	
 				FirebaseOptions options = FirebaseOptions.builder()
 						.setCredentials(GoogleCredentials.fromStream(serviceAccount))
 	                    .setDatabaseUrl("https://notifycheck-dfc1f.firebaseio.com").build();
@@ -37,7 +25,8 @@ public class FirebaseService {
 	            FirebaseApp.initializeApp(options);
 	        }
 	        	
-	    }
+    }
+	  
 	 public Firestore getFirebase(){
      	return FirestoreClient.getFirestore();
      }
